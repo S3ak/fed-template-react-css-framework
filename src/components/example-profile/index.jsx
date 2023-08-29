646f8c73d48cf684a62b9e3fc4b30c0bd0b0c30e
Monde Sineke <nsineke@gmail.com>
2023-08-29 10:47:17 +0200

    ✨ feat(example-profile): User profile
    Add more details

--- src/components/example-profile/index.jsx
+++ src/components/example-profile/index.jsx
@@ -18,7 +18,18 @@ function ExampleProfile() {
   return (
     <>
       _PROFILE_CONTENT_GOES_HERE_
+      <h1>
+        {data.firstName} {data.lastName}
+      </h1>
+      <p>Eye color is {data.eyeColor}</p>
       <img src={data.image} alt="avatar" />
+      <h2>Bio</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+        sed aliquam temporibus fugiat in accusantium sint ipsum sequi
+        consequuntur, necessitatibus vitae iusto facere officia dolorum nobis.
+        Deleniti veniam voluptates dolores.
+      </p>
     </>
   );
 }
